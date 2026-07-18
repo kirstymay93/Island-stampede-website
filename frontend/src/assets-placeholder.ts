6c870b162fdeca0493baf207d44b4d5a88392b08
island-stampede <islandstampede26@gmail.com>
2026-07-18 14:29:24 +1000

    Add placeholder image URIs to avoid missing-asset build failures

---
+++ frontend/src/assets-placeholder.ts
@@ -0,0 +1,9 @@
+const IMAGES = {
+  hero: { uri: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="1200" height="800"><rect width="100%" height="100%" fill="%230a44ff"/></svg>' },
+  gallery1: { uri: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600"><rect width="100%" height="100%" fill="%230a44ff"/></svg>' },
+  gallery2: { uri: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600"><rect width="100%" height="100%" fill="%23000000"/></svg>' },
+  gallery3: { uri: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600"><rect width="100%" height="100%" fill="%23ffffff"/></svg>' },
+  gallery4: { uri: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="800" height="600"><rect width="100%" height="100%" fill="%23222222"/></svg>' },
+};
+
+export default IMAGES;
